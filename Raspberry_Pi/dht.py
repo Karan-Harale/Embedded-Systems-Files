b576f6f57a9fa1c88147b4ebc443d17913c0a212
Karan <138441706+Karan-Harale@users.noreply.github.com>
2024-05-09 12:17:13 +0530

    Update dht.py

--- Raspberry_Pi/dht.py
+++ Raspberry_Pi/dht.py
@@ -1,14 +1,4 @@
-'''
-sudo apt-get install git-core
 
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-
-cd Adafruit_Python_DHT
-
-sudo apt-get install build-essential python-dev
-
-sudo python setup.py install
-'''
 #Connect Data Pin DHT11 Sensor in GPIO26 of Raspberry Pi
 import Adafruit_DHT
 while True:
